b55eea488fb78902fd05abf9760cacabce5992fa
Noor Ul Islam <noorulislam770@gmail.com>
2024-05-27 10:12:55 +0500

    working with header and how to handle request and responses, and adding headers to the response object and the ending it

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import http from "http";
 
 function onRequest(req, res) {
   console.log(`recieved a request : ${req.method} ${req.url}`);
+
+  res.setHeader("Content-Type", "Application/json");
+  res.writeHead(200, {
+    // "Content-Type": "application/json",
+    "X-something": "Hello",
+  });
+  console.log("page not found");
+  res.write("hello !! ");
   res.end("hello from node server");
 }
 
